8277a812c955f4afd1b3b9914e03af4717c0bcc4
Yuren Ju <yurenju@gmail.com>
2015-06-15 01:21:25 +0800

    Removed amd detection

--- angular-file-upload.js
+++ angular-file-upload.js
@@ -3,13 +3,7 @@
  https://github.com/nervgh/angular-file-upload
 */
 (function(angular, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define('angular-file-upload', ['angular'], function(angular) {
-            return factory(angular);
-        });
-    } else {
-        return factory(angular);
-    }
+    return factory(angular);
 }(typeof angular === 'undefined' ? null : angular, function(angular) {
 
 var module = angular.module('angularFileUpload', []);
